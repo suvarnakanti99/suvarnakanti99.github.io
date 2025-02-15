39aba473724ae6cb8c262e73311ce769e2d349dc
suvarnakanti99 <156829325+suvarnakanti99@users.noreply.github.com>
2025-02-14 18:55:26 -0600

    Update email.js

--- email.js
+++ email.js
@@ -5,8 +5,8 @@ function showOrHideEmail(){
     visible=false;
   }
   else{
-    var myEmail="<a href='mailto:suvarnrk" +"@"+
-                "mail.uc.edu'>suvarnrk"+"@"+"mail.uc.edu</a>";
+    var myEmail="<a href='mailto:ruthviksuvarnakanti" +"@"+
+                "gmail.com'>ruthviksuvarnakanti"+"@"+"gmail.com</a>";
 
     document.getElementById('mailId').innerHTML=myEmail;
     visible= true;
